b6f0996d666e9dc47c1cb724d87b03004b89c278
SanjoDB <1006110438@u.icesi.edu.co>
2024-11-06 12:08:57 -0500

    Content card modified

--- src/components/content/ContentCard.tsx
+++ src/components/content/ContentCard.tsx
@@ -4,13 +4,26 @@ interface ContentCardProps {
     type: string;
   }
   
-  const ContentCard = ({ id, title, type }: ContentCardProps) => {
+const ContentCard = ({ id, title, type }: ContentCardProps) => {
     return (
-      <div className="border rounded-lg shadow-lg p-4 bg-white w-full max-w-md">
-        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
-        <p className="text-sm text-gray-600">{type}</p>
+      <div className="border rounded-lg shadow-lg bg-white w-full max-w-md">
+        {/* Imagen en la parte superior */}
+        <div
+          className="h-48 rounded-t-lg"
+          style={{
+            backgroundImage: `url("https://via.placeholder.com/300")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
+        
+        {/* Contenido debajo de la imagen */}
+        <div className="p-4">
+          <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+          <p className="text-sm text-gray-600">{type}</p>
+        </div>
       </div>
     );
-  };
+};
   
-  export default ContentCard;  
+export default ContentCard;  
